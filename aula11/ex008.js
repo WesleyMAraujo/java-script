b7423739d9d5969b713e9d521490ff1449b16db4
Wesley Monteiro de Araujo <wesleymonteirodearaujo@gmail.com>
2023-03-31 22:09:54 -0300

    att

--- aula11/ex008.js
+++ aula11/ex008.js
@@ -1,1 +1,21 @@
-console.log('o console funcionou corretamente')
+/* Exemplo de if else em js usando o node js */
+
+var media = 6
+
+/* estrutura de decisão ou condições */
+if (media > 7 && media < 11) {
+    console.log(`Você esta aprovado`)
+} else if (media < 7){
+    console.log(`Você esta reprovado`)
+} else {
+    console.log(`MEDIA ACIMA DE 10 ERROR`)
+}
+
+var vel = 71
+var velmax = 70
+if (vel > velmax) {
+    console.log(`Você esta acima da velocidade permitida da rodovia de ${velmax} sua velocidade é: ${vel}`)
+} else {
+    console.log(`sua velocidade esta entre normal`)
+}
+
---
+++ aula11/ex009.js
@@ -0,0 +1,6 @@
+var pais = 'EUA'
+if (pais == 'Brasil') {
+    console.log("BRASILEIRO")
+} else {
+    console.log("ESTRANGEIRO")
+}
