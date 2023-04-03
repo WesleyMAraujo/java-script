d82d9f955873d36594b019174312e3faed69c2c2
Wesley Monteiro de Araujo <wesleymonteirodearaujo@gmail.com>
2023-04-03 15:01:35 -0300

    att

---
+++ exercicios/at002/script.js
@@ -0,0 +1,12 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var imprimir = document.getElementById('imprimir')
+    if (fano.value.length == 0 || fano.value > ano) {
+        alert('Verifique os dados novamente')
+    } else {
+        var fsex = docu
+    }
+    
+}
