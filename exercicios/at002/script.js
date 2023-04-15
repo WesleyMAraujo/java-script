7e9d4791f407a8b3b7b1e5dc48fe5ed010b5d721
Wesley Monteiro de Araujo <wesleymonteirodearaujo@gmail.com>
2023-04-15 12:59:24 -0300

    Update script.js

--- exercicios/at002/script.js
+++ exercicios/at002/script.js
@@ -3,10 +3,25 @@ function verificar() {
     var ano = data.getFullYear()
     var fano = document.getElementById('txtano')
     var imprimir = document.getElementById('imprimir')
-    if (fano.value.length == 0 || fano.value > ano) {
+
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
         alert('Verifique os dados novamente')
     } else {
-        var fsex = docu
+        var fsex = document.getElementsByName("radsex")
+        var idade = ano - Number(fano.value)
+
+        var genero = ``
+        if (fsex[0].checked) {
+            genero = 'Homem'
+        } else {
+            genero = 'Mulher'
+        }
+        imprimir.style.textAlign = 'center'
+        if (genero == 'Homem') {
+            imprimir.innerHTML = `Você é ${genero} com ${idade} anos <br> <img src="imagens/foto-jovem-m.png">`
+        } else {
+            imprimir.innerHTML = `Você é ${genero} com ${idade} anos <br> <img src="imagens/foto-jovem-f.png">`
+        }
     }
     
 }
